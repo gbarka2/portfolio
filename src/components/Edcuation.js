00c4cb7f93f0afdd113cc4c42b72cf286029ebc8
Gary Barkauski <gbarka2@gmail.com>
2021-03-15 11:48:29 -0600

    Add work history component

--- src/components/Edcuation.js
+++ src/components/Edcuation.js
@@ -2,7 +2,18 @@ import React from "react"
 
 const Education = () => {
   return (
-    <h1>Education component</h1>
+    <div>
+      <article>
+        <p>December 2020 - March 2021</p>
+        <p>General Assembly</p>
+        <p>Software Development Immersive</p>
+      </article>
+      <article>
+        <p>August 2018 - May 2012</p>
+        <p>University of Illinois Urbana-Champaign</p>
+        <p>Bachelor of Arts - Political Science & History</p>
+      </article>
+    </div>
   )
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Title = () => {
   return (
-    <h1>Title component</h1>
+    <h1>Gary Barkauski</h1>
   )
 }
 
