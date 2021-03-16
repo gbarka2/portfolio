a5f2ec9527676a93075e22e8a49fddf975d9f9c9
Gary Barkauski <gbarka2@gmail.com>
2021-03-16 10:47:37 -0600

    Add video autoplay

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,16 @@
 import React from "react"
+import "./Project.css"
 
 const Project = (props) => {
+  console.log(props.project)
   return (
-    <h1>{props.project.name}</h1>
+    <div>
+      <video id="project-video" width="640" height="480" autoPlay muted>
+        <source src={props.project.video} type="video/mp4">
+        </source>
+      </video>
+      <h1>{props.project.name}</h1>
+    </div>
   )
 }
 
